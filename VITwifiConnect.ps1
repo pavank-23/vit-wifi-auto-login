d9f70c4cb16d455dd94c68044cd9e0171d4cf528
Pavan <23pavank@gmail.com>
2022-12-03 00:18:45 +0530

    second commit

---
+++ VITwifiConnect.ps1
@@ -0,0 +1,9 @@
+﻿$webRequest = Invoke-WebRequest -Uri 'http://phc.prontonetworks.com/cgi-bin/authlogin' -SessionVariable webSession
+$loginForm  = $webRequest.Forms[0]
+[string]$userName = '21BDS0198'
+[string]$userPassword = 'RP576Z'
+[securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
+[pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $secStringPassword)
+$loginForm.Fields.userId   = $credObject.UserName
+$loginForm.Fields.password = $credObject.GetNetworkCredential().Password
+$webRequest = Invoke-WebRequest -Uri 'http://phc.prontonetworks.com/cgi-bin/authlogin' -Method $loginForm.Method -Body $loginForm.Fields -WebSession $webSession
