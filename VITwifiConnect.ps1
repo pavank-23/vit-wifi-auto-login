3cb737c74f837d2590a6a151f5c650ff8f413aa8
Pavan K <87761045+pavank-23@users.noreply.github.com>
2024-04-01 18:14:26 +0530

    Update VITwifiConnect.ps1

--- VITwifiConnect.ps1
+++ VITwifiConnect.ps1
@@ -1,9 +1,14 @@
-﻿$webRequest = Invoke-WebRequest -Uri 'http://phc.prontonetworks.com/cgi-bin/authlogin' -SessionVariable webSession
-$loginForm  = $webRequest.Forms[0]
-[string]$userName = ''
-[string]$userPassword = ''
+$webRequest = Invoke-WebRequest -Uri 'http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://captive.apple.com/hotspot-detect.html' -SessionVariable webSession
+Write-Output($webRequest)
+Write-Output($webRequest.Forms)
+[string]$userName = '21BDS0198'
+[string]$userPassword = 'RP576Z'
 [securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
 [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $secStringPassword)
-$loginForm.Fields.userId   = $credObject.UserName
-$loginForm.Fields.password = $credObject.GetNetworkCredential().Password
-$webRequest = Invoke-WebRequest -Uri 'http://phc.prontonetworks.com/cgi-bin/authlogin' -Method $loginForm.Method -Body $loginForm.Fields -WebSession $webSession
+$loginForm = @{
+    userId = $credObject.UserName 
+    password = $credObject.GetNetworkCredential().Password
+}
+$webRequest = Invoke-WebRequest -Uri 'http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://captive.apple.com/hotspot-detect.html' -Method Post -Body $loginForm -WebSession $webSession
+
+Write-Output($webRequest.Content)
